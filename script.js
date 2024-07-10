09a933f629a95f0c6034acc2b9bb8c49b6daa138
Bluflax <13920567537@163.com>
2024-07-10 16:40:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function parseAndDisplayData(rawData) {
             // Apply fade-in animation with delay
             setTimeout(() => {
                 imtopicdiv.classList.add('fade-in');
-            }, Math.floor(index / 2) * 60);
+            }, Math.floor(index / 2) * 10);
         });
 
     } catch (error) {
