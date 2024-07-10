fe87c8b9321a460658e3a96c7dc229c0937673fb
Bluflax <13920567537@163.com>
2024-07-10 18:15:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function parseAndDisplayData(rawData) {
             });
         }, { 
             rootMargin: '20px', // 提前 20px 触发
-            threshold: 1
+            threshold: 0
         });
 
         // 观察所有 imtopic 元素
